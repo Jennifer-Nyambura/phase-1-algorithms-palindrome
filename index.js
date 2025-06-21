be14cd0872b75e33b148f7d22337623a03d09256
Jennifer <jenninyambu@gmail.com>
2025-06-21 09:33:50 +0300

    all changes

--- index.js
+++ index.js
@@ -1,25 +1,53 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  // Reverse the word
+  const reversedWord = word.split("").reverse().join("");
+
+  // Check if original and reversed words are the same
+  return word === reversedWord;
 }
 
 /* 
-  Add your pseudocode here
+  Pseudocode:
+  - Take the word
+  - Split it into characters
+  - Reverse the characters
+  - Join them back into a string
+  - Compare the reversed string to the original
+  - Return true if equal, else false
 */
 
 /*
-  Add written explanation of your solution here
+  Explanation:
+  A palindrome is a word that reads the same forwards and backwards.
+  By reversing the string and comparing it to the original, 
+  we can determine if it's a palindrome.
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
+  // custom test cases
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
   console.log("");
 
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("ab"));
 }
 
 module.exports = isPalindrome;
